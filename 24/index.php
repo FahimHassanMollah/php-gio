fb2411287a106c674149494919253c5cd252bfc4
fahimhassanmollah <fahimhassan0197@gmail.com>
2024-10-07 22:19:59 +0600

    added scoped related changes in 24

--- 24/index.php
+++ 24/index.php
@@ -3,6 +3,55 @@
 declare(strict_types=1);
 
 /*
+// variable scopes
+$x = 10.36;
+
+function test()
+{
+    // global $x;
+    
+    // $x= $GLOBALS['x'];
+    $x= $GLOBALS['x'] = 100;
+    echo $x;
+}
+echo $x;
+echo "<br>";
+test();
+*/
+
+
+/*
+
+// static variable
+
+function getValue ()
+{
+    static $value = null;
+    if ($value === null) {
+        $value = (function () {
+            echo "Calculating value...<br>";
+            sleep(1);
+            return 1;
+        })();
+    }
+    
+    return $value;
+}
+echo getValue();
+echo "<br>";
+echo getValue();
+echo "<br>";
+echo getValue();
+
+*/
+
+
+
+
+
+
+
+/*
 
 
 $sum1 = 'sums';
@@ -21,6 +70,11 @@ function sums(...$arg){
 */
 
 
+
+
+
+/*
+
 // anonymous function
 $global = 10;
 $sum = function (...$arg) use ($global) {
@@ -30,7 +84,7 @@ $sum = function (...$arg) use ($global) {
 $sum();
 
 
-
+*/
 
 
 
